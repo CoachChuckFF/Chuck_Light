e7261d827537bc55dec9405c786108816737d9e1
jeichenhofer <joseph.eichenhofer@gmail.com>
2017-10-04 13:00:35 -0500

    verilog testbench successful

--- SoC/ece453/ip/ece453/ece453_tb.sv
+++ SoC/ece453/ip/ece453/ece453_tb.sv
@@ -22,7 +22,6 @@ module ece453_tb();
 
 	initial begin
 		clk = 1'b0;
-		rst = 1'b0;
 		forever begin
 			#10 clk = ~clk;
 		end
@@ -30,6 +29,7 @@ module ece453_tb();
 
 	initial begin
 		// initialize signals for test
+		rst = 1'b1;
 		addr = 4'b0;
 		read_en = 1'b0;
 		write_en = 1'b0;
@@ -39,8 +39,70 @@ module ece453_tb();
 
 		// wait a quarter clock cycle so changes happen before edge
 		#5;
-
-		$finish;
+		
+		// deassert reset after one clock cycle
+		#20;
+		rst = 1'b0;
+		
+		#20;
+		
+		// write all 0x00ff_ffff to LED 0
+		write_data = 32'h00FF_FFFF;
+		addr = 4'h8;
+		write_en = 1'b1;
+		#20;
+		// write all 1s to LED 1
+		write_data = 32'h0011_1111;
+		addr = 4'h9;
+		write_en = 1'b1;
+		#20;
+		// write all 2s to LED 2
+		write_data = 32'h0022_2222;
+		addr = 4'ha;
+		write_en = 1'b1;
+		#20;
+		// write all 3s to LED 3
+		write_data = 32'h0033_3333;
+		addr = 4'hb;
+		write_en = 1'b1;
+		#20;
+		// write all 4s to LED 4
+		write_data = 32'h0044_4444;
+		addr = 4'hc;
+		write_en = 1'b1;
+		#20;
+		// write all 5s to LED 5
+		write_data = 32'h0055_5555;
+		addr = 4'hd;
+		write_en = 1'b1;
+		#20;
+		// write all 6s to LED 6
+		write_data = 32'h0066_6666;
+		addr = 4'he;
+		write_en = 1'b1;
+		#20;
+		// write all 7s to LED 7
+		write_data = 32'h0077_7777;
+		addr = 4'hf;
+		write_en = 1'b1;
+		#20;
+		
+		// enable interrupts
+		write_data = 32'h0000_0001;
+		addr = 4'h3;
+		#20;
+		
+		
+		// send out signals
+		write_data = 32'b1;
+		addr = 4'h1;
+		write_en = 1'b1;
+		#20;
+		write_en = 1'b0;
+		
+		@(posedge irq_out);
+		
+		$stop;
 	end
 
 endmodule
