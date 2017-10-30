d4df7d7e38583ee6cee0e2f43651eaa2a0273b48
unknown <Joseph Eichenhofer@DESKTOP-4SG18JC.CS.WISC.EDU>
2017-10-30 13:07:40 -0500

    created eclipse project for java server. added defines as consts

---
+++ debug_server/src/chuck/threads/HeartBeatThread.java
@@ -0,0 +1,11 @@
+package chuck.threads;
+
+import java.net.DatagramSocket;
+
+public class HeartBeatThread extends Thread {
+	protected DatagramSocket socket = null;
+	
+	public HeartBeatThread(int port) {
+		
+	}
+}
