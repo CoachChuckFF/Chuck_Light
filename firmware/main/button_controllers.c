aee913ecf73433361644a07b71d0c28929b7106e
Christian Krueger <christian@blizzardlighting.com>
2017-11-17 17:21:41 -0600

    pull up resistors

--- firmware/main/button_controllers.c
+++ firmware/main/button_controllers.c
@@ -19,7 +19,7 @@ void init_button_controllers()
     io_conf.mode = GPIO_MODE_INPUT;
     io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
     io_conf.pull_down_en = 0;
-    io_conf.pull_up_en = 0;
+    io_conf.pull_up_en = 1;
     gpio_config(&io_conf);
 
 }
